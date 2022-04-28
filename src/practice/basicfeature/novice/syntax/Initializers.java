f710b80b9feb5e98f2aea9f053979e84952fdab7
mickeyjap182 <yoshitaka_8an9drums@msn.com>
2022-04-29 04:00:14 +0900

    fix initialize practice

--- src/practice/basicfeature/novice/syntax/Initializers.java
+++ src/practice/basicfeature/novice/syntax/Initializers.java
@@ -5,6 +5,9 @@ import practice.utils.logger.TimeLogger;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 製造業 Initializer practice for Manufacturing industry.
+ */
 public class Initializers extends ParentInitializers {
 
     /** constructor as dynamic initializer */
@@ -48,7 +51,42 @@ public class Initializers extends ParentInitializers {
         logger.info(String.format("message: %s num: %d ",lastStatusMessage ,num++));
     }
 
+    /** static member */
     private static String[] learnTask;
+    /** instance field */
+    private String currentTerm;
+
+    enum TaxPattern {
+        PER_UNIT, // ユニット単価ごとに税率算出
+        AFTER_AMOUNT;  // 最後に算出
+    }
+    public class Rot {
+        private final String rotSerialCode;
+        private final long unitSize;
+        public Rot(String pRotSerialCode, int pUnitSize) {
+            rotSerialCode = pRotSerialCode;
+            unitSize = pUnitSize;
+        }
+    }
+    /** inner class for calculating Price. */
+    public strictfp class Price {
+        private final String title;
+        private final double taxLate;
+        private long unitPrice;
+        private Rot rot;
+        private double value;
+
+        public Price(double pTaxLate) {
+            title = this.getClass().getName();
+            taxLate = pTaxLate;
+        }
+        public void calc(long pUnitPrice, Rot pRot) {
+            unitPrice = pUnitPrice;
+            rot = pRot;
+            value = unitPrice * num;
+        }
+    }
+
 }
 class ParentInitializers {
     protected static int num = 1;
@@ -68,3 +106,23 @@ class ParentInitializers {
 
 }
 
+class Employee {
+    private List<Role> roles;
+    private final String givenName;
+    private final String middleName;
+    private final String familyName;
+    /** TODO parts*/
+    private transient List<String> socialSecurityNumber = new ArrayList<>();
+    /** TODO */
+    private volatile long id = 102L;
+    public Employee(String pGivenName, String pMiddleName, String pFamilyName) {
+        givenName = pGivenName;
+        middleName = pMiddleName;
+        familyName = pFamilyName;
+    }
+
+}
+
+class Role {
+
+}
